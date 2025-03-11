ec5249caec7d0ed6d8a934efa57009258011f66a
lucas philippe <lucas.philippe.nunes@gmail.com>
2025-03-10 21:03:17 -0300

    feat: cast shadows

---
+++ Scripts/cast_shadow.gd
@@ -0,0 +1,15 @@
+class_name CastShadow extends Node2D
+
+@onready var raycast: RayCast2D = get_node("RayCast2D")
+@onready var pointlight: PointLight2D = get_node("PointLight2D")
+
+var rayCollisionPoint: float = 0.0
+
+func _physics_process(_delta: float) -> void:
+  if raycast.is_colliding():
+    rayCollisionPoint = raycast.get_collision_point().y
+  else:
+    rayCollisionPoint = raycast.global_position.y + raycast.target_position.y
+
+  pointlight.global_position.y = rayCollisionPoint
+  pointlight.energy = max(1.0 - pointlight.position.y / raycast.target_position.y, 0.0)
