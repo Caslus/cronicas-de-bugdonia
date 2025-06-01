a6dabccccbf52bd505c1aed04ee53442c016f713
lucas philippe <lucas.philippe.nunes@gmail.com>
2025-06-01 17:53:58 -0300

    fix: allow dialog with click

--- Scripts/Utils/Interactable.gd
+++ Scripts/Utils/Interactable.gd
@@ -5,12 +5,14 @@ extends Node2D
 @onready var exclamation: Sprite2D = get_node("Exclamation")
 @onready var exclamationAnimationPlayer: AnimationPlayer = exclamation.get_node("AnimationPlayer")
 @onready var area: Area2D = get_node("Area2D")
+@onready var player: CharacterBody2D = get_tree().get_nodes_in_group("player")[0]
 
 signal interactable(npc)
 
 func _ready():
 	area.connect("body_entered", Callable(self, "_on_body_entered"))
 	area.connect("body_exited", Callable(self, "_on_body_exited"))
+	area.connect("input_event", Callable(self, "_on_input_event"))
 	exclamationAnimationPlayer.connect("animation_finished", Callable(self, "_on_animation_finished"))
 
 
@@ -21,6 +23,10 @@ func _on_body_entered(body):
 func _on_body_exited(body):
 	if body.name == "Player":
 		deactivate_interactable()
+
+func _on_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		player.get_node("InteractionManager").start_interaction()
 
 func activate_interactable():
 	interactable.emit(self.get_parent())
