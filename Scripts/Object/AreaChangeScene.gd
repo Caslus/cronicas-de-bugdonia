6fd8cfd3bc29fd77551717205d27ea74cf269435
lucas philippe <lucas.philippe.nunes@gmail.com>
2025-09-01 14:49:05 -0300

    feat: add boolean logic UI assets and a gate to enter level 2

---
+++ Scripts/Object/AreaChangeScene.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var next_scene: String = "res://Scenes/Example.tscn"
+
+func _on_body_entered(body):
+	print("entered")
+	if body.name == "Player":
+		TransitionCanvas.transition()
+		await TransitionCanvas.on_transition_finished
+		get_tree().change_scene_to_file(next_scene)
