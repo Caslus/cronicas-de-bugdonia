10d8d90be565ec397eb7d0912ec19eceb45cec58
lucas philippe <lucas.philippe.nunes@gmail.com>
2025-09-07 17:27:54 -0300

    feat: add smoke

--- Scripts/Object/Fireball.gd
+++ Scripts/Object/Fireball.gd
@@ -2,11 +2,21 @@ extends Node2D
 
 @export var speed: float = 400.0
 @export var lifetime: float = 2.0
+@export var explodeParticles: PackedScene
 var velocity: Vector2 = Vector2.ZERO
 var time_alive: float = 0.0
+
+func spawn_explosion() -> void:
+		var particles = explodeParticles.instantiate()
+		particles.position = position
+		get_tree().root.add_child(particles)
+		particles.emitting = true
+		await get_tree().create_timer(particles.lifetime).timeout
+		particles.queue_free()
 
 func _process(delta):
 		position += velocity * delta
 		time_alive += delta
 		if time_alive > lifetime:
-				queue_free()
+			spawn_explosion()
+			queue_free()
